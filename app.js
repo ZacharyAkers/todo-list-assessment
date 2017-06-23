9882ee1e5c9c3528b10fa82d49a893e4be76eba4
ZacharyAkers <user@users-air.lan>
2017-06-23 08:52:24 -0400

    redo

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
@@ -11,17 +12,29 @@ app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
 
-const todos = [];
+let toDo = [
 
-app.get("/", function(req, res) {
-  res.render("index", { todos: todos });
+];
+
+let completed = [];
+
+
+app.get("/", function(req, res){
+  res.render("index", {toDo: toDo, completed: completed});
 });
 
-app.post("/", function (req, res) {
-  todos.push(req.body.todo);
-  res.redirect('/');
+app.post("/", function(req, res){
+  if(req.body.newTodo){
+  toDo.push(req.body.newTodo);
+  res.redirect("/");
+  }
+  else {
+    toDo.splice(toDo.indexOf(req.body.incomplete), 1);
+    completed.push(req.body.incomplete)
+    res.redirect("/")
+  }
 });
 
-app.listen(3000, function() {
+app.listen(3000, function(){
   console.log("App is running on localhost:3000");
 });
